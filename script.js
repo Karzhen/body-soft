07b61d56b2df8b6cf86bdc07db5ff4b58e625c3c
karzhen <egor9298@mail.ru>
2023-10-02 22:25:39 +0300

    faet: complete the arrows logic in services LANG=en_EN Mon, Oct 02, 2023 10:25:39 PM

--- script.js
+++ script.js
@@ -9,58 +9,53 @@ const arrowLeft = document.querySelector('.left'),
 let slideIndex = 0,
     previousSlide = 0;
 
+const checkArrows = () => {
+    if (slideIndex === 0) {
+        arrowLeft.classList.add('hidden');
+    } else if (slideIndex === arrayImages.length - 1) {
+        arrowRight.classList.add('hidden');
+    }
+}
+
 const checkInactive = (currentSlide) => {
     if (0 < currentSlide && currentSlide < arrayImages.length) {
         arrowLeft.classList.remove('hidden')
         arrowRight.classList.remove('hidden')
     }
 }
+
+const containerServices = (previous, current) => {
+    massageContainer[previous].classList.add('none');
+    massageContainer[current].classList.remove('none');
+    arrowRight.classList.add('arrow-right');
+    arrowLeft.classList.add('arrow-left');
+    checkInactive(current);
+}
+
 const nextSlide = () => {
     if (slideIndex < (arrayImages.length - 1)) {
-        console.log(slideIndex);
         slideIndex += 1;
-        massageContainer[previousSlide].classList.add('none');
-        massageContainer[slideIndex].classList.remove('none');
-        // arrayImages[previousSlide].classList.add('none');
-        // arrayImages[previousSlide].classList.remove('visible');
-        // arrayImages[slideIndex].classList.remove('none');
-        // arrayImages[slideIndex].classList.add('visible');
-        arrowRight.classList.remove('arrow-right-inactive');
-        arrowRight.classList.add('arrow-right');
-        arrowLeft.classList.add('arrow-left');
-        checkInactive(slideIndex)
+        containerServices(previousSlide, slideIndex);
         previousSlide += 1;
     } else {
         arrowRight.classList.remove('arrow-right');
-        // arrowRight.classList.add('arrow-right-inactive');
         arrowRight.classList.add('hidden')
     }
-    // arrayImages[slideIndex].style.border  = '2px solid black';
-    console.log(previousSlide);
+    checkArrows();
 }
 
 const prevSlide = () => {
     if (slideIndex > 0) {
         slideIndex -= 1;
-        massageContainer[previousSlide].classList.add('none');
-        massageContainer[slideIndex].classList.remove('none');
-        // arrayImages[previousSlide].classList.add('none');
-        // arrayImages[previousSlide].classList.remove('visible');
-        // arrayImages[slideIndex].classList.remove('none');
-        // arrayImages[slideIndex].classList.add('visible');
-        arrowLeft.classList.remove('arrow-left-inactive');
-        arrowLeft.classList.add('arrow-left');
-        arrowRight.classList.add('arrow-right');
-        checkInactive(slideIndex)
+        containerServices(previousSlide, slideIndex);
         previousSlide -= 1;
     } else {
         arrowLeft.classList.remove('arrow-left');
-        // arrowLeft.classList.add('arrow-left-inactive');
         arrowLeft.classList.add('hidden')
     }
-    // arrayImages[slideIndex].style.border  = '2px solid black';
-    console.log(previousSlide);
+    checkArrows();
 }
 
+window.addEventListener('load', checkArrows);
 arrowLeft.addEventListener('click', prevSlide);
 arrowRight.addEventListener('click', nextSlide);
