dde95daa1b34a06d6a27c99317d07c39ac0a0d84
Egor Cherkashin <egor9298@mail.ru>
2023-10-18 21:16:56 +0300

    feat: add the links into check-our-inst section LANG=en_EN Wed, Oct 18, 2023 09:16:56 PM

---
+++ slider.js
@@ -0,0 +1,19 @@
+// const reviewsWrapper = document.querySelector('.reviews-wrapper');
+// const reviewContainers = document.querySelectorAll('.review-container');
+// let reviewIndex = 0;
+//
+// const updateSlider = () => {
+//     reviewsWrapper.style.transform = `translateX(${-reviewIndex * 25}%)`;
+// }
+//
+// const autoSlide = () => {
+//     if (reviewIndex < reviewContainers.length - 4) {
+//         reviewIndex++;
+//     } else {
+//         reviewIndex = 0; // Обнулить индекс для бесконечного слайдера
+//     }
+//     updateSlider();
+// }
+//
+// // Устанавливаем интервал автоматической прокрутки каждые 5 секунд
+// setInterval(autoSlide, 5000);
